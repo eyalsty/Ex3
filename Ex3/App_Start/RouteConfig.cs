d5fda90ab431b6c428bbd6b51ae364d13143b13d
eyal styskin <eyal.styskin@live.biu.ac.il>
2019-05-25 22:29:15 +0300

    Add project files.

---
+++ Ex3/App_Start/RouteConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Ex3
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("displayOnce", "display/{ip}/{port}",
+            new { controller = "First", action = "DisplayOnce" });
+
+            routes.MapRoute("autoDisplay", "display/{ip}/{port}/{rate}",
+            new { controller = "First", action = "AutoDisplay" });
+
+            routes.MapRoute("saveDisplay", "save/{ip}/{port}/{rate}/{duration}/{fname}",
+            new { controller = "First", Action = "SaveDisplay" });
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "First", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Ex3/Controllers/FirstController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ex3.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult DisplayOnce(string ip, int port)
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AutoDisplay(string ip, int port)
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SaveDisplay(string ip, int port)
+        {
+
+            return View();
+        }
+    }
+}
